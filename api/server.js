f96b35f34a2d665ca929045d3ea833136e9b3b52
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-05-20 22:28:12 -0400

    MVP

--- api/server.js
+++ api/server.js
@@ -4,6 +4,81 @@ const db = require("../data/dbConfig.js");
 
 const server = express();
 
-server.use(express.json());
+const validateId = (req, res, next) => {
+  db('accounts').where({ id: req.params.id})
+  .then(response => {
+    if (response.length !== 0) {
+      next()
+    } else {
+      res.status(400).json({ message: "invalid id" })
+    }
+    
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).send(error)
+  })
+}
 
+const validateNewAccount = (req, res, next) => {
+  if (!("name" in req.body) || !("budget" in req.body)) {
+    res.status(400).json({ message: "missing required field(s)"})
+  } else {
+    next()
+  }
+}
+
+server.use(express.json());
+server.use('/:id', validateId)
+server.post('/', validateNewAccount)
+
+server.get('/', (req, res) => {
+  db('accounts')
+  .then(response => {
+    res.status(200).send(response)
+  })
+  .catch(error => {
+    res.status(500).send(error)
+  })
+})
+
+server.get('/:id', (req, res) => {
+  db('accounts').where({ id: req.params.id})
+  .then(response => {
+    res.status(200).send(response)
+  })
+  .catch(error => {
+    res.status(500).send(error)
+  })
+})
+
+server.post('/', (req, res) => {
+  db('accounts').insert(req.body)
+  .then(response => {
+    res.status(201).send(response)
+  })
+  .catch(error => {
+    res.status(500).send(error)
+  })
+})
+
+server.put('/:id', (req, res) => {
+  db('accounts').where({ id: req.params.id }).update(req.body)
+  .then(response => {
+    res.status(201).json(response)
+  })
+  .catch(error => {
+    res.status(500).send(error)
+  })
+})
+
+server.delete('/:id', (req, res) => {
+  db('accounts').where({ id: req.params.id }).delete()
+  .then(response => {
+    res.status(201).json(response)
+  })
+  .catch(error => {
+    res.status(500).send(error)
+  })
+})
 module.exports = server;
